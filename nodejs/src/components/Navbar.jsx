11514576602b479addf1d7a032fdb68fcbb8b1f3
dmitriy <dmitriy@bessalov.ru>
2021-03-28 13:24:59 +0300

    aunf MUI token

--- nodejs/src/components/Navbar.jsx
+++ nodejs/src/components/Navbar.jsx
@@ -16,13 +16,13 @@ export const NavMenu = () => {
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
-    if (useMediaQuery('(min-width:1024px)')){
+    //if (useMediaQuery('(min-width:1024px)')){
     return (
     <Container>
         <Link to="/">
-            <Button variant="contained" style={{"margin": "10px"}}>Home</Button>
+            <Button variant="contained" style={{"margin": "10px"}}>Правила игры</Button>
         </Link>
-        <Button variant="contained" style={{"margin": "10px"}}>Раздел</Button>
+        <Button variant="contained" style={{"margin": "10px"}}>Онлайн игра</Button>
         <Button aria-describedby={id} variant="contained" style={{"margin": "10px"}} onClick={handleClick}>
             Меню авторизации
         </Button>
@@ -34,41 +34,37 @@ export const NavMenu = () => {
             anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
             transformOrigin={{vertical: 'top', horizontal: 'center'}}
         >
-            <Link to="/aunf/login">
-                <MenuItem>Login</MenuItem>
+            <Link to="/auth/users">
+                <MenuItem>Создание пользователя</MenuItem>
             </Link>
-            <Link to="/aunf/reset_password">
-                <MenuItem>ResetPassword</MenuItem>
+            <Link to="/auth/users/activation">
+                <MenuItem>Активация</MenuItem>
             </Link>
-            <Link to="/aunf/users">
-                <MenuItem>RegisterUser</MenuItem>
+            <Link to="/auth/users/resend_activation">
+                <MenuItem>Повторить активацию</MenuItem>
             </Link>
-            <Link to="/aunf/users/activation">
-                <MenuItem>Activation</MenuItem>
+            <Link to="/auth/login">
+                <MenuItem>Авторизация</MenuItem>
             </Link>
-            <Link to="/aunf/users/resend_activation">
-                <MenuItem>ResendActivation</MenuItem>
+            <Link to="/auth/reset_password">
+                <MenuItem>Сбросить пароль</MenuItem>
             </Link>
-            <Link to="aunf/users/reset_password">
-                <MenuItem>ResetActivation</MenuItem>
+            <Link to="/auth/logout">
+                <MenuItem>Выход</MenuItem>
             </Link>
-            <Link to="/aunf/logout">
-                <MenuItem>Logout</MenuItem>
-            </Link>
-
         </Popover>
     </Container>
     )
-    }else{
-        return (
-          <AppBar>
-            <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-               <MenuIcon />
-            </IconButton>
-            </Toolbar>
-          </AppBar>
-        )
-    }
+    // }else{
+    //     return (
+    //       <AppBar>
+    //         <Toolbar>
+    //         <IconButton edge="start" color="inherit" aria-label="menu">
+    //            <MenuIcon />
+    //         </IconButton>
+    //         </Toolbar>
+    //       </AppBar>
+    //     )
+    //}
 }
 
