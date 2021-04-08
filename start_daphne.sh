fee487be2cf135ab616f6148d21c3bc425de2f3b
dmitriy <dmitriy@bessalov.ru>
2021-04-08 16:13:14 +0300

    Добавлен асинхронный режим

--- start_daphne.sh
+++ start_daphne.sh
@@ -1,4 +1,3 @@
 #!bin/bash
 cd /var/www/social_network
-source ./.venv/bin/activate
-exec daphne -b 0.0.0.0 -p 8000 config.asgi:application
+source ./.venv/bin/activate && exec daphne -b 0.0.0.0 -p 8000 config.asgi:application
