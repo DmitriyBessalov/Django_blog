c44847b46bbcd2a83cfd3dc0136ea4d64f2e1a0d
dmitriy <dmitriy@bessalov.ru>
2021-04-12 12:21:19 +0300

    collectstatic

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('users2', include('apps.users.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api', include('apps.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
