c0aa700be8716d03f529caf9983ff987674a15a1
dmitriy <dmitriy@bessalov.ru>
2021-04-12 21:00:27 +0300

    -

--- apps/mainapp/views.py
+++ apps/mainapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
     return render(request, 'index.html', {})
 
 
+@csrf_exempt
 def git_pull(request):
     os.system('git reset --hard origin/master && git pull https://github.com/DmitriyBessalov/social_network.git')
     return HttpResponse('True')
-
