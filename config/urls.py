23bc9c73801b9134d54635250532b322279fcb46
dmitriy <dmitriy@bessalov.ru>
2021-04-12 22:12:40 +0300

    csrf_exempt

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from apps.mainapp.views import index, git_pull
-from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api', include('apps.urls')),
-    path('gitpull', csrf_exempt(git_pull)),
+    path('gitpull', git_pull),
     path('auth/', index),
     path('', index),
 ]
